4b4b59f47001e2b01cffa11d05a7fc21db5ef9b5
lucasrpb <Ghb7140#@!>
2023-02-22 12:08:51 -0300

    working version.

--- src/main/scala/demo/Reconciler.scala
+++ src/main/scala/demo/Reconciler.scala
@@ -2,16 +2,16 @@ package demo
 
 import scala.util.Failure
 import scala.util.Success
-
 import akka.actor.ActorSystem
 import akka.persistence.cassandra.reconciler.Reconciliation
+import com.typesafe.config.ConfigFactory
 
 object Reconciler {
 
   def main(args: Array[String]): Unit = {
     // System should have the same Cassandra plugin configuration as your application
     // but be careful to remove seed nodes so this doesn't join the cluster
-    val system = ActorSystem()
+    val system = ActorSystem("Reconciler", ConfigFactory.load("application2.conf"))
     import system.dispatcher
 
     val rec = new Reconciliation(system)

1848ef64cd6f53beb171b979bd9074db51e83c96
lucasrpb <Ghb7140#@!>
2023-02-22 11:42:10 -0300

    working version.

---
+++ src/main/scala/demo/Reconciler.scala
@@ -0,0 +1,30 @@
+package demo
+
+import scala.util.Failure
+import scala.util.Success
+
+import akka.actor.ActorSystem
+import akka.persistence.cassandra.reconciler.Reconciliation
+
+object Reconciler {
+
+  def main(args: Array[String]): Unit = {
+    // System should have the same Cassandra plugin configuration as your application
+    // but be careful to remove seed nodes so this doesn't join the cluster
+    val system = ActorSystem()
+    import system.dispatcher
+
+    val rec = new Reconciliation(system)
+    val result = rec.rebuildAllPersistenceIds()
+
+    result.onComplete {
+      case Success(_) =>
+        system.log.info("All persistenceIds migrated.")
+        system.terminate()
+      case Failure(e) =>
+        system.log.error(e, "All persistenceIds migration failed.")
+        system.terminate()
+    }
+  }
+
+}

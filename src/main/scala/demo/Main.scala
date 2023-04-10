4611dd5ab3d5df43f60a86e9f0ce3c36cabf5d4c
lucasrpb <lucasrpb@gmail.com>
2023-04-09 22:18:52 -0300

    working version.

--- src/main/scala/demo/Main.scala
+++ src/main/scala/demo/Main.scala
@@ -16,6 +16,7 @@ object Main extends App {
     "CounterPersistentActor")
 
   counterPersistentActor ! Command(Increment(3))
+  counterPersistentActor ! Command(Increment(3))
   counterPersistentActor ! Checkpoint
 
  // Thread.sleep(1000)
